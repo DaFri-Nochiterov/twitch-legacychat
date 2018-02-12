d5b4248de28bb61168866cfc7784617da4d83234
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2018-02-13 04:08:37 +0700

    trying to avoid using jquery + adapting to twitch changes

--- twitch-legacychat.user.js
+++ twitch-legacychat.user.js
@@ -9,30 +9,32 @@
 // @grant        none
 // ==/UserScript==
 
-const initChat = (username) => {
+const SLEEP_BEFORE_INIT = 5000;
+
+const createChatFrame = (username) => {
     if (!username) { throw new Error("No username passed"); }
-    const column = $("div.channel-page__right-column")[0];
+
+    const frame = document.createElement("iframe");
+
+    frame.setAttribute("src", `https://www.twitch.tv/${username}/chat?popout=`);
+
+    frame.style.height = "100%";
+    frame.classList.add("legacychat_frame");
+
+    return frame;
+};
+
+const initChat = (username) => {
+    const column = document.querySelectorAll("div[data-test-selector=chat-room-component-layout]")[0];
     if (!column) { return console.error("[LegacyChat] Failed to get chat column"); }
-    let chatFrame = $("<iframe>").attr({
-        src: `https://www.twitch.tv/${username}/chat?popout=`,
-        class: "legacychat_frame"
-    }).css({
-        height: "100%"
-    });
-    console.info("[LegacyChat] Inserting chat into column. Here magic happens");
-    $(column).html(chatFrame);
+    column.innerHTML = createChatFrame(username).outerHTML;
+    console.info("[LegacyChat] Done creating legacy chat frame");
 };
 
 const getChannelUsername = () => {
-    const header = $("a[data-a-target='user-channel-header-item']")[0];
-    if (!header) { throw new Error("Header item not found"); }
-    let uri = header.href;
-    uri = uri.slice("https://".length);
-    const slashes = uri.match(/\//g); // finding slashes as they point if it's videos or not
-    // then checking this 'point'
-    if (slashes && slashes.length > 1) { throw new Error("It's not a channel"); }
-    uri = uri.slice(uri.indexOf("/") + 1);
-    return uri;
+    const uriUsername = location.pathname.match(/^\/([a-z0-9\_]{3,24})/);
+    if(!uriUsername || uriUsername.length !== 2) { throw new Error("Could not find username in `pathname`"); }
+    return uriUsername[1];
 };
 
 const init = () => {
@@ -40,56 +42,15 @@ const init = () => {
     try {
         username = getChannelUsername();
     } catch (err) {
-        return console.error("[LegacyChat] Failed to get channel username, header was removed or something broken?", err);
+        return console.error("[LegacyChat] Failed to get channel username:", err);
     }
-    if (!username) { return console.error("[LegacyChat] We could not find username. WOW, RUDE!"); }
+    if (!username) { return console.error("[LegacyChat] No username returned"); }
     console.info("[LegacyChat] Initializing legacy chat for username", username, "may took a seconds");
     initChat(username);
 };
 
-const initDBLClick = () => {
-    // shitcode of adding double clicks to mention
-    const msgBox = $(".chat_text_input")[0];
-    const linesEl = $($(".chat-lines")[0]);
-
-    if (!linesEl) { return console.warn("[LegacyChat] Cannot find chat box!"); }
-
-    linesEl.on("DOMNodeInserted", "li", (e) => {
-        const target = $(e.target);
-        if (!target) { return console.warn("[LegacyChat] Wowee, stange error here. #nulTrgt"); }
-        const from = $(target.find("span.from")[0]);
-        let clicks = 0,
-            cltmo;
-        from.on("click", () => {
-            clicks++;
-
-            const mention = `@${from[0].innerText}`;
-            if (!cltmo) {
-                cltmo = setTimeout(() => {
-                    clicks = 0;
-                    cltmo = undefined;
-                }, 1500);
-            }
-
-            if ((clicks % 2) === 0) {
-                if (msgBox.value.includes(mention)) { return; } // anitdoubledouble click, heck
-                msgBox.value += msgBox.value.length === 0 ? `${mention} ` : ` ${mention}`; //padding
-                setTimeout(() => $(".moderation-card__close-button").click(), 100); // closing mod card
-            }
-        });
-    });
-};
-
-(() => {
-    console.info("[LegacyChat] Hello o/");
-    console.info("[LegacyChat] Scheduling chat initialization in 5 seconds");
-    if (!location.pathname.endsWith("/chat")) {
-        setTimeout(() => init(), 5000);
-    } else {
-        if("BetterTTV" in window) {
-            console.info("[LegacyChat] Detected BTTV, not going to do anything with chat");
-            return;
-        }
-        setTimeout(() => initDBLClick(), 5000);
-    }
-})();
+console.info("[LegacyChat] Hello o/");
+console.info("[LegacyChat] Scheduling chat initialization in 5 seconds");
+if (location.hostname === "www.twitch.tv" && !location.pathname.endsWith("/chat")) {
+    setTimeout(() => init(), SLEEP_BEFORE_INIT);
+}
